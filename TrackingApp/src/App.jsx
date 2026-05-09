7731b25b4a40736d8a00b6830088546fd32b52a5
Kiet <tuankiet9804@gmail.com>
2026-05-09 10:16:06 +0700

    Hotupdate

--- TrackingApp/src/App.jsx
+++ TrackingApp/src/App.jsx
@@ -8,7 +8,6 @@ import Dashboard from './pages/Dashboard'
 import TransactionsPage from './pages/TransactionsPage'
 import ReportsPage from './pages/ReportsPage'
 import OnboardingPage from './pages/OnboardingPage'
-import BudgetPage from './pages/BudgetPage'
 import { getTransactions } from './services/transactionService'
 import { getAllWallets } from './services/walletService'
 
@@ -154,8 +153,6 @@ function App() {
         )
       case 'reports':
         return <ReportsPage wallets={wallets} transactions={transactions} />
-      case 'budget':
-        return <BudgetPage />
       case 'wallets':
         return (
           <WalletManager
